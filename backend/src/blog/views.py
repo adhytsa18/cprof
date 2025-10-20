aa4211b32f47d5de43281db82fec6e46d36c0696
ppf <ppf@ppfs-MacBook-Air.local>
2025-10-20 11:16:33 +0700

    tambah repo ke github

--- backend/src/blog/views.py
+++ backend/src/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def blog_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/list.html', {'posts': posts})
--- backend/src/cprof/urls.py
+++ backend/src/cprof/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('appc.urls')), 
+    path('blog/', include('blog.urls')),
 ]
